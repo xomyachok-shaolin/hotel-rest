82c5891d20e8a7caefb0c70b7a27e19281e755fe
xomyachok-shaolin <56404900+xomyachok-shaolin@users.noreply.github.com>
2021-12-07 10:08:49 +0500

    Update initial_sql_script.sql

--- initial_sql_script.sql
+++ initial_sql_script.sql
@@ -1,8 +1,16 @@
-create table users_table (
-    id serial not null
-                         constraint users_table_pk
-                         primary key ,
-                         name varchar(50),
-                         login varchar(50),
-                         email varchar(40)
-)
+insert into "roles" values(1, 'USER');
+insert into "roles" values(2, 'ADMIN');
+
+insert into "type" values(1, 'Люкс');
+insert into "type" values(2, 'Делюкс');
+insert into "type" values(3, 'Семейный');
+
+
+insert into "room" values(1, 1, 199, '111', 3);
+insert into "room" values(2, 2, 499, '220', 1);
+insert into "room" values(3, 2, 399, '201', 2);
+
+insert into "service" values(1, 1999, 'Лобстеры и шампанское', 'Питание');
+insert into "service" values(2, 499, 'Бизнес ланч', 'Питание');
+insert into "service" values(3, 199, 'Завтрак и уборка номера', 'Все');
+insert into "service" values(4, 399, 'Стирка и глажка белья', 'Обслуживание');
